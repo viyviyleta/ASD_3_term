3499b00aa96f5c9c3b4ed8775cceedad77b6d932
Violetta <153115117+viyviyleta@users.noreply.github.com>
2024-09-09 17:47:05 +0300

    Add files via upload

---
+++ 1lab/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void hanoiTower(int n, int i, int k) {
+    if (n == 1) {
+        cout << "Переместить диск 1 с " << i << " на " << k << " стержень" << endl;
+    }
+    else {
+        int temporary = 6 - i - k;
+        hanoiTower(n - 1, i, temporary);
+        cout << "Переместить диск " << n << " с " << i << " на " << k << " стержень" << endl;
+        hanoiTower(n - 1, temporary, k);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n, i, k;
+
+    cout << "Введите количество дисков n: ";
+    cin >> n;
+
+    if (n < 1) {
+        cout << "Ошибка! n может быть числом больше илии равным 1" << endl;
+        return 1;
+    }
+
+    cout << "Введите номер начального стержня i (от 1 до 3): ";
+    cin >> i;
+
+    if (i < 1 || i > 3) {
+        cout << "Ошибка! Номер стержня должен быть от 1 до 3" << endl;
+        return 1;
+    }
+
+    cout << "Введите номер стержня k, на который надо переместить пирамиду (от 1 до 3): ";
+    cin >> k;
+
+    if ( k < 1 || k > 3) {
+        cout << "Ошибка! Номер стержня должен быть от 1 до 3" << endl;
+        return 1;
+    }
+
+    if (i == k) {
+        cout << "Ошибка! Начальный и конечный стержни должны быть разными" << endl;
+        return 1;
+    }
+
+    hanoiTower(n, i, k);
+
+    return 0;
+}
