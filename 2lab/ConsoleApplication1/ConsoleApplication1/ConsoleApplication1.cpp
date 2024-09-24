4f17a34453624c9218257743d9c833bb84fdf7b7
Violetta <153115117+viyviyleta@users.noreply.github.com>
2024-09-24 18:37:13 +0300

    Add files via upload

---
+++ 2lab/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,157 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+const int N = 10;  //кол-во вершин графа
+bool* visited = new bool[N];  //посещенные вершинах
+
+struct Edge { 
+	int start;  //начало ребра (вершина)
+	int finish;  //конец ребра (вершина)
+};
+
+// Матрица смежности графа
+int graph[N][N] = {
+	{0, 1, 0, 0, 1, 0, 0, 0, 0, 0}, 
+	{1, 0, 0, 0, 0, 0, 1, 1, 0, 0},  
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 0},  
+	{0, 0, 0, 0, 0, 1, 0, 0, 1, 0},  
+	{1, 0, 0, 0, 0, 1, 0, 0, 0, 0},  
+	{0, 0, 0, 1, 1, 0, 0, 0, 1, 0},  
+	{0, 1, 0, 0, 0, 0, 0, 1, 0, 0},  
+	{0, 1, 1, 0, 0, 0, 1, 0, 0, 0},  
+	{0, 0, 0, 1, 0, 1, 0, 0, 0, 1},  
+	{0, 0, 0, 0, 0, 0, 0, 0, 1, 0}   
+};
+
+//обход графа в ширину (BFS)
+void BFS(int start) {
+	queue<int> q;  
+	bool visited[N]{};  //посещенные вершин
+	bool inqueue[N]{};  //добавленые в очередь
+	int view;  //текущая вершина
+
+	for (int i = 0; i < N; i++) {
+		visited[i] = inqueue[i] = false;
+	}
+
+	q.push(start);
+	visited[start] = inqueue[start] = true;  
+
+	//пока очередь не пуста, продолжаем обход
+	while (!q.empty()) {
+		view = q.front();
+		cout << view + 1 << " ";  
+		visited[view] = true; 
+		q.pop();  
+
+		for (int i = 0; i < N; i++) {
+			if (!inqueue[i] && graph[view][i]) {  
+				q.push(i);  
+				inqueue[i] = true;  
+			}
+		}
+	}
+}
+
+//обход графа в глубину (DFS)
+void DFS(int start) {
+	stack<int> s;  
+	bool visited[N]{};  
+	bool inqueue[N]{};  
+	int view;  
+
+	for (int i = 0; i < N; i++) {
+		visited[i] = inqueue[i] = false;
+	}
+
+	s.push(start);
+	visited[start] = inqueue[start] = true; 
+
+	while (!s.empty()) {
+		view = s.top(); 
+		cout << view + 1 << " "; 
+		visited[view] = true; 
+		s.pop();  
+
+		for (int i = 0; i < N; i++) {
+			if (!inqueue[i] && graph[view][i]) {  
+				s.push(i);  
+				inqueue[i] = true;  
+			}
+		}
+	}
+}
+
+void main() {
+	setlocale(LC_ALL, "rus"); 
+	cout << "Список смежности: " << endl;
+
+	vector<Edge> edges = {
+		{1, 2}, {2, 1}, {1, 5}, {5, 1},  
+		{2, 7}, {7, 2}, {2, 8}, {8, 2},  
+		{7, 8}, {8, 7}, {8, 3}, {3, 8},
+		{5, 6}, {6, 5}, {4, 6}, {6, 4},
+		{4, 9}, {9, 4}, {6, 9}, {9, 6},
+		{9, 10}, {10, 9} 
+	};
+
+	int n = 11;  
+	vector<vector<int>> adjList;  // Список смежности
+	adjList.resize(n);  
+
+	for (auto& edge : edges) {
+		adjList[edge.start].push_back(edge.finish);  
+	}
+
+	for (int i = 1; i < n; i++) {
+		cout << i << "--> { ";  
+		for (int v : adjList[i]) {
+			cout << v << " ";  
+		}
+		cout << "}" << endl;
+	}
+	cout << endl;
+
+	cout << "Список ребер: " << endl;
+	for (int i = 1; i < n; i++) {
+		for (int v : adjList[i]) {
+			cout << "{" << i << "," << v << "}"; 
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	int start;  
+	cout << "Матрица смежности графа: " << endl;
+
+	//выводим матрицу смежности графа
+	for (int i = 0; i < N; i++) {
+		visited[i] = false;  // Инициализация посещенных вершин
+		for (int j = 0; j < N; j++)
+			cout << " " << graph[i][j];  
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << "Стартовая вершина: ";
+	cin >> start; 
+	if (start > 10 || start < 1) cout << "Ошибка";
+	else {
+		cout << "\n\nПорядок обхода в ширину: ";
+		BFS(start - 1);
+		cout << endl;
+		delete[]visited;
+		cout << "\nПорядок обхода в глубину: ";
+		DFS(start - 1);
+		cout << endl;
+		cout << endl;
+	}
+	//cout << "Расчёт О-большое: " << endl;
+	//int v = 10, e = 11;
+	//cout << "O(|V| + |E|) = " << abs(v) + abs(e) << endl;  
+}
---
+++ 3lab/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int V = 9;
+
+void Dijkstra(int GR[V][V], int st);
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int start;
+	int GR[V][V] =
+	{
+		/*A*/ {0,  7,  10, 0,  0,  0,  0,  0,  0},
+		/*B*/ {7,  0,  0,  0,  0,  9,  27, 0,  0},
+		/*C*/ {10, 0,  0,  0,  31, 8,  0,  0,  0},
+		/*D*/ {0,  0,  0,  0,  32, 0,  0,  17, 21},
+		/*E*/ {0,  0,  31, 32, 0,  0,  0,  0,  0},
+		/*F*/ {0,  9,  8,  0,  0,  0,  0,  11, 0},
+		/*G*/ {0,  27, 0,  0,  0,  0,  0,  0,  15},
+		/*H*/ {0,  0,  0,  17, 0,  11, 0,  0,  15},
+		/*I*/ {0,  0,  0,  21, 0,  0,  15, 15, 0},
+		     /*A   B   C   D   E   F   G   H   I*/
+	};
+
+	char symbols[40];
+	bool pr = false;
+	printf("Введите начальную верщшину: ");
+
+	while (pr == false)
+	{
+		cin >> symbols;
+		if (symbols[1] == '\0' && (symbols[0] == 'A' || symbols[0] == 'B' || symbols[0] == 'C' ||
+			symbols[0] == 'D' || symbols[0] == 'E' || symbols[0] == 'F' ||
+			symbols[0] == 'G' || symbols[0] == 'H' || symbols[0] == 'I'))
+			pr = true;
+		else
+			printf("Ошибка!");
+	}
+	int letter = (int)symbols[0] - 65;
+	Dijkstra(GR, letter);
+}
+
+void Dijkstra(int GR[V][V], int st)
+{
+	int length[V], index, u, m = st + 1;
+	bool visited[V];
+	for (int i = 0; i < V; i++)
+	{
+		length[i] = INT_MAX;
+		visited[i] = false;
+	}
+	length[st] = 0;
+	for (int count = 0; count < V - 1; count++)
+	{
+		int min = INT_MAX;
+
+		for (int i = 0; i < V; i++)
+			if (!visited[i] && length[i] <= min)
+			{
+				min = length[i];
+				index = i;
+			}
+		u = index;
+		visited[u] = true;
+
+		for (int i = 0; i < V; i++)
+			if (!visited[i] && GR[u][i] && length[u] != INT_MAX && length[u] + GR[u][i] < length[i])
+				length[i] = length[u] + GR[u][i];
+	}
+
+	printf("длина маршрутов:\n");
+
+	for (int i = 0; i < V; i++)
+	{
+		if (length[i] != INT_MAX)
+			printf("Из вершины %c в %c : %d\n", (char)(m + 64), (char)(i + 65), length[i]);
+		else
+			printf("Из вершины %c в %c : недоступен\n", (char)(m + 64), (char)(i + 65));
+	}
+}
